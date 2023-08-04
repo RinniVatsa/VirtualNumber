97ef498cacdb8240e1da8c6150af40cba8bf28e9
Rinni Vatsa <rinnivatsa09@gmail.com>
2023-08-04 18:17:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,94 +5,68 @@ import React, { useState } from 'react';
 import './App.css';
 import { countriesData } from './countriesData';
 
-
-
-
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-  const toggleNav = () => {
-    setIsOpen(!isOpen);
-  };
+    const toggleNav = () => {
+        setIsOpen(!isOpen);
+    };
 
-  return (
-    <div>
-      {/* Animated Header */}
-      <header className={`animated-header ${isOpen ? 'open' : ''}`} onClick={toggleNav}>
-        <div className="container">
-          <h3>Virtual Numbers</h3>
-        </div>
-      </header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-          <button
-            className="navbar-toggler"
-            type="button"
-            onClick={toggleNav}
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <a className="nav-link" href="#home">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#about">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#services">
-                  Services
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#contact">
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    return ( <
+        div > { /* Animated Header */ } <
+        header className = { `animated-header ${isOpen ? 'open' : ''}` }
+        onClick = { toggleNav } >
+        <
+        div className = "container" >
+        <
+        h3 > Virtual Number < /h3> < /
+        div > <
+        /header>
 
-      {/* Navigation */}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        {/* ... rest of the navigation code */}
-      </nav>
+        { /* Navigation */ } <
+        nav className = "navbar navbar-expand-lg navbar-light bg-light" > { /* ... rest of the navigation code */ } <
+        /nav>
 
-      {/* Main Content */}
-      <main>
-        <div className="container mt-4">
-          <div className="row">
-            {countriesData.map((country, index) => (
-              <div key={index.id} className="col-md-4 mb-4">
-                {/* <CountryCard country={country} /> */}
-                <div className="card">
-      <img src={country.image} className="card-img-top" alt={country.name} />
-      <div className="card-body">
-        <h5 className="card-title">{country.name}</h5>
-        <p className="card-text"> Number: {country.Number}</p>
-      </div>
-    </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </main>
+        { /* Main Content */ } <
+        main >
+        <
+        div className = "container mt-4" >
+        <
+        div className = "row" > {
+            countriesData.map((country, index) => ( <
+                div key = { index }
+                className = "col-md-4 mb-4" >
+                <
+                div className = "card" >
+                <
+                img src = { country.image }
+                className = "card-img-top"
+                alt = { country.name }
+                /> <
+                div className = "card-body" >
+                <
+                h5 className = "card-title" > { country.name } < /h5> <
+                p className = "card-text" > Number: { country.Number } < /p> < /
+                div > <
+                /div> < /
+                div >
+            ))
+        } <
+        /div> < /
+        div > <
+        /main>
 
-      {/* Footer */}
-      <footer className="bg-light p-3 mt-4">
-        <div className="container text-center">
-          <p>&copy; {new Date().getFullYear()} Virtual Number. All rights reserved.</p>
-        </div>
-      </footer>
-    </div>
-  );
+        { /* Footer */ } <
+        footer className = "bg-light p-3 mt-4" >
+        <
+        div className = "container text-center" >
+        <
+        p > & copy; { new Date().getFullYear() }
+        Virtual Number.All rights reserved. < /p> < /
+        div > <
+        /footer> < /
+        div >
+    );
 };
 
 export default App;
--- src/countriesData.js
+++ src/countriesData.js
@@ -1,83 +1,81 @@
-
 export const countriesData = [
 
     {
-        id:1,
-      name: 'INDIA',
-      image:require('./Images/india.png') ,
-      Number: "+91 ",
+        id: 1,
+        name: 'INDIA',
+        image: require('./Images/india.png'),
+        Number: "+91 767899987",
     },
     {
-        id:2,
-      name: 'United States',
-      image: require('./Images/us.png') ,
-      Number:"+1 (515) 516-0624",
+        id: 2,
+        name: 'United States',
+        image: require('./Images/us.png'),
+        Number: "+1 (515) 516-0624",
     },
 
     {
-        id:3,
+        id: 3,
         name: 'Uae',
-        image: require('./Images/Uae.png') ,
-        Number:"+1 (515) 516-0624",
-      },
+        image: require('./Images/Uae.png'),
+        Number: "+1 (515) 516-0624",
+    },
 
-      {
-        id:4,
+    {
+        id: 4,
         name: 'France',
-        image: require('./Images/france.jpeg') ,
-        Number:"+1 (515) 516-0624",
-      },
-      {
-        id:5,
+        image: require('./Images/france.jpeg'),
+        Number: "+1 (515) 516-0624",
+    },
+    {
+        id: 5,
         name: 'Canada',
-        image: require('./Images/c.png') ,
-        Number:"+1 (515) 516-0624",
-      },
-      {
-        id:6,
+        image: require('./Images/c.png'),
+        Number: "+1 (515) 516-0624",
+    },
+    {
+        id: 6,
 
         name: 'United States',
-        image: require('./Images/india.png') ,
-        Number:"+1 (515) 516-0624",
-      },
-      {
-        id:8,
+        image: require('./Images/india.png'),
+        Number: "+1 (515) 516-0624",
+    },
+    {
+        id: 8,
         name: 'Romania',
-        image: require('./Images/ro.png') ,
-        Number:"+40731883509",
-      },
-      {
-        id:7,
+        image: require('./Images/ro.png'),
+        Number: "+40731883509",
+    },
+    {
+        id: 7,
         name: 'Philippines',
-        image: require('./Images/ph.png') ,
-        Number:"+639669917700",
-      },
-      {
-        id:7,
+        image: require('./Images/ph.png'),
+        Number: "+639669917700",
+    },
+    {
+        id: 7,
         name: 'Spain',
-        image: require('./Images/spa.png') ,
-        Number:"(+34) 631-281-895",
-      },
-      {
-        id:7,
+        image: require('./Images/spa.png'),
+        Number: "(+34) 631-281-895",
+    },
+    {
+        id: 7,
         name: 'Australia',
-        image: require('./Images/aust.png') ,
-        Number:"(+61) 431-759-236",
-      },
+        image: require('./Images/aust.png'),
+        Number: "(+61) 431-759-236",
+    },
 
-      {
-        id:8,
+    {
+        id: 8,
         name: '	Estonia',
-        image: require('./Images/est.png') ,
-        Number:"(+372) 5363-1003",
-      },
+        image: require('./Images/est.png'),
+        Number: "(+372) 5363-1003",
+    },
 
-      {
-        id:9,
+    {
+        id: 9,
         name: 'Swedish',
-        image: require('./Images/istockphoto-472317387-612x612.jpeg') ,
-        Number:"+46769436266",
-      },
+        image: require('./Images/istockphoto-472317387-612x612.jpeg'),
+        Number: "+46769436266",
+    },
     // Add more country objects as needed
-  ];
- 
+];
